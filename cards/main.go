a5278f1e87aa6b19991c1f5c71e643c3f6aec3bf
jrich8573 <jasonrich85@icloud.com>
2022-10-11 17:12:55 -0400

    updated cards main.go file

--- cards/main.go
+++ cards/main.go
@@ -5,10 +5,15 @@ import "fmt"
 func main() {
 
 	// identical assignments
-	//var card string = "Ace of Spades"
-	card := newCard()
+	// var card string = "Ace of Spades"
+	// card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()} // new cards
+	cards = append(cards, "Six of Spades")          // add a card to cards
 
-	fmt.Println(card)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	fmt.Println(cards)
 }
 
 func newCard() string { // string return type
@@ -27,4 +32,8 @@ Notes:
 	d. float64 (10.0003, 0.09999, -100.3)
 4. only use the := when assigning a new variable (when reassigning a variable use the = )
 5. go allows you to print the return value of a function without assigning it to a var
+6. go has 2 data structures for handling lists of data.
+	a. arrary = fixed length list of data
+	b. slice = an array that can grow and shrink
+7. in a slice = every element must be of the same type
 */
